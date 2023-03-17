bf4c7650bef87d9f26d616f8d6c2cadd658ddbf7
lorenzobellino <lor.bellino@gmail.com>
2023-03-17 13:15:41 +0100

    solution for lab2

--- labs/lab2/lab2_pandas.py
+++ labs/lab2/lab2_pandas.py
@@ -2,6 +2,7 @@ import logging
 import matplotlib.pyplot as plt
 import pandas as pd
 import itertools
+import argparse
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -9,7 +10,14 @@ logging.basicConfig(level=logging.DEBUG)
 FILENAME = "iris.csv"
 
 
-def scatterplots(irisSetosa, irisVersicolor, irisVirginica, columns):
+def scatterplots(
+    irisSetosa: pd.DataFrame,
+    irisVersicolor: pd.DataFrame,
+    irisVirginica: pd.DataFrame,
+    columns: pd.DataFrame,
+    root: str,
+    save: bool = True,
+) -> None:
     for c1, c2 in itertools.combinations(columns[:-1], 2):
         plt.figure()
         plt.scatter(x=irisSetosa[c1], y=irisSetosa[c2], alpha=0.5, label="Iris-setosa")
@@ -25,29 +33,42 @@ def scatterplots(irisSetosa, irisVersicolor, irisVirginica, columns):
         plt.legend()
         plt.xlabel(c1)
         plt.ylabel(c2)
-        plt.savefig(f"plots/scatter_{c1}_{c2}.png")
+        if save:
+            plt.savefig(f"plots/standard_pandas/scatter_{c1}_{c2}.png")
+        else:
+            plt.show()
         plt.close()
 
 
-def histograms(irisSetosa, irisVersicolor, irisVirginica, columns):
+def histograms(
+    irisSetosa: pd.DataFrame,
+    irisVersicolor: pd.DataFrame,
+    irisVirginica: pd.DataFrame,
+    columns: pd.DataFrame,
+    root: str,
+    save: bool = True,
+) -> None:
     for c in columns[:-1]:
         plt.hist(x=irisSetosa[c], bins=10, alpha=0.5, label="Iris-setosa")
         plt.hist(x=irisVersicolor[c], bins=10, alpha=0.5, label="Iris-versicolor")
         plt.hist(x=irisVirginica[c], bins=10, alpha=0.5, label="Iris-virginica")
         plt.legend()
         plt.xlabel(c)
-        plt.savefig(f"plots/hist_{c}.png")
+        if save:
+            plt.savefig(f"{root}hist_{c}.png")
+        else:
+            plt.show()
         plt.close()
 
 
-def statistics(iris):
+def statistics(iris: pd.DataFrame, save: bool) -> None:
     for c in iris.columns[:-1]:
         print(f"Statistics for {c}")
         print(iris[c].describe())
         print("")
 
 
-def visualization():
+def visualization(save=True) -> None:
     iris = pd.read_csv(FILENAME)
     columns = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
     iris.columns = columns
@@ -55,10 +76,30 @@ def visualization():
     irisSetosa = iris[iris["species"] == "Iris-setosa"]
     irisVersicolor = iris[iris["species"] == "Iris-versicolor"]
     irisVirginica = iris[iris["species"] == "Iris-virginica"]
-    # histograms(irisSetosa, irisVersicolor, irisVirginica, columns)
-    # scatterplots(irisSetosa, irisVersicolor, irisVirginica, columns)
-    statistics(iris)
+    # histograms(
+    #     irisSetosa,
+    #     irisVersicolor,
+    #     irisVirginica,
+    #     columns,
+    #     "plots/standard_pandas/",
+    #     save,
+    # )
+    # scatterplots(
+    #     irisSetosa,
+    #     irisVersicolor,
+    #     irisVirginica,
+    #     columns,
+    #     "plots/standard_pandas/",
+    #     save,
+    # )
+    statistics(iris, save)
 
 
 if __name__ == "__main__":
-    visualization()
+    parser = argparse.ArgumentParser(
+        description="lab2 : datset visualization and statistics on Iris Datatset"
+    )
+    parser.add_argument("-s", "--save", help="save the plots", action="store_true")
+    args = parser.parse_args()
+    visualization(args.save)
+    # visualization()
